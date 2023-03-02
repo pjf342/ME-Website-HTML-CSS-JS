4472f3c6447503f9344fcfaa71d7ec5498a773c2
Patrick <patrick.fleckenstein@outlook.com>
2023-03-01 23:43:25 -0800

    added new portfolio script

---
+++ javascript/portfolio-script.js
@@ -0,0 +1,19 @@
+function openPortfolio(event, portfoliocontent) {
+    var i, tabcontent, tablink;
+
+    tabcontent = document.getElementsByClassName("tabcontent");
+    for (i = 0; i < tabcontent.length; i++) {
+        tabcontent[i].style.display = "none";
+    }
+
+    tablink = document.getElementsByClassName("tablink");
+    for (i = 0; i < tablink.length; i++) {
+        tablink[i].className = tablink[i].className.replace(" active","");
+    }
+
+    document.getElementById(portfoliocontent).style.display = "flex";
+    event.currentTarget.className += " active";
+}
+
+
+const tab = document.getElementById("defaulttab").click(); 
