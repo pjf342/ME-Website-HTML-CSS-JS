35ea49f8687db66869ec16097b20abeba5e27c0f
Patrick <patrick.fleckenstein@outlook.com>
2023-03-02 13:44:48 -0800

    added functional principal stresses javascript

--- javascript/ps-script.js
+++ javascript/ps-script.js
@@ -1,18 +1,42 @@
-// Query the submit button
-const btn = document.querySelector('#submit-btn');
+const button = document.getElementById('submit-btn').addEventListener('click', principalStresses);
 
-const button = document.getElementById('submit-btn')
-                        .addEventListener('click', testfn);
+function principalStresses() {
+    var sX, sY, tXY, sAvg, s1, s2, phi1, phi2, ccw, theta1, theta2, sXprime, tMax, shearPhi1, shearPhi2;
 
+    sX = Math.floor(document.getElementById('sigmaX').value); //stress in x axis (horizontal right is positive)
+    sY = Math.floor(document.getElementById('sigmaY').value); //stress in y axis (vertical up is positive)
+    tXY = Math.floor(document.getElementById('tauXY').value); //shear
 
-function testfn() {
-    var sigmaX, sigmaY, tauXY;
-    sigmaX = Math.floor(document.getElementById('sigmaX').value);
-    sigmaY = Math.floor(document.getElementById('sigmaY').value);
-    multiple = sigmaX * sigmaY;
-    console.log(multiple);
+    sAvg = (sX + sY) / 2;
+    s1 = sAvg + Math.sqrt(Math.pow(((sX - sY) / 2), 2) + (Math.pow(tXY,2))); //first principal stress
+    s2 = sAvg - Math.sqrt(Math.pow(((sX - sY) / 2), 2) + (Math.pow(tXY,2))); //second principal stress
+    
+    phi1 = radiansToDegrees(Math.atan(tXY / ((sX - sY) / 2))) / 2; //angle to reach principal stress 1
+    phi2 = phi1 + 90; //angle to reach principal stress 2
+    theta1 = phi1 * 2;
+    theta2 = phi2 * 2;
+    ccw = rotation(phi1);
+    sXprime = (sX + sY / 2) + (((sX - sY) / 2) + Math.cos(2 * degreesToRadians(phi1)))
+    + (tXY * Math.sin(2 * degreesToRadians(phi1)));
+
+    tMax = Math.sqrt(Math.pow(((sX - sY) / 2), 2) + (Math.pow(tXY,2))); //in-plane shear stress
+    shearPhi1 = radiansToDegrees(Math.atan(-(((sX - sY) / 2) / tXY))) / 2; //angle to reach max shear stress 1
+    shearPhi2 = shearPhi1 + 90; //angle to reach max shear stress 1
+    console.log(shearPhi1, shearPhi2);
+} 
+
+function radiansToDegrees(radians){
+    return radians / (Math.PI / 180);
+}
+function degreesToRadians(degrees){
+    return degrees * (Math.PI / 180);
+}
+function rotation(angle){
+    return angle > 0;
 }
 
+// Query the submit button
+// const btn = document.querySelector('#submit-btn');
 
 // Query all boxes
 // const boxes = document.querySelectorAll('.box');
