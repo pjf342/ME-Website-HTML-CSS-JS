1beb5becffcf8aec5060b8aa2ac7fa9418f8e337
Patrick <patrick.fleckenstein@outlook.com>
2023-04-11 18:01:18 -0700

    added functional universal files for head, header, footer

--- javascript/script.js
+++ javascript/script.js
@@ -1,1 +1,7 @@
+always();
 
+async function always() {
+    document.getElementById('header').innerHTML += await (await fetch('header.html')).text();
+    document.getElementById('footer').innerHTML += await (await fetch('footer.html')).text();
+    document.getElementById('head').innerHTML += await (await fetch('head.html')).text();
+}
